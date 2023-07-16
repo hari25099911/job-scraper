4a6d6483776f74b1b85d64370da574631853aa63
Srihariharasudhan Balakannan <93313179+hari25099911@users.noreply.github.com>
2023-07-16 22:22:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,148 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+# imports
+import pandas as pd
+import plotly.express as px
 import streamlit as st
-from selenium.webdriver.common.by import By
+import plotly.graph_objects as go
+import plotly.graph_objs as gobj
+from scrape_data import get_job_details
+from functions import roll_up_data, add_coordinates
 
-def get_driver():
-    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox')  
-options.add_argument('--disable-dev-shm-usage')
-driver = get_driver()
+# set page configuration
+st.set_page_config(page_title="Trends in Data jobs (India)", page_icon=":bar_chart:", layout="wide")
 
+# title
 st.title('Trends in Data jobs')
-job_role = st.selectbox('Select a job role?', ("Data Engineer", "Data Analyst", "Data Architect", "Data Scientist", "Machine Learning Engineer"))
-lst = job_role.split(' ')
-str = ""
-c = 0
-for i in lst:
-    i = i.strip()
-    if c == 0:  
-        str+=i
-        c+=1
-    else:
-        str+='+'+i
-        c+=1
-page = f'https://www.foundit.in/srp/results?query="{str}"'
-page = page.encode('ascii', 'ignore').decode('unicode_escape')
-driver.get(page)
-count = 0
-while count < 50:
-    elements = driver.find_elements(By.XPATH, "/html/body/div[@id='srpThemeDefault']/div[@class='srpContainer']/div[@id='srpContent']/div[@class='srpCardContainer']/div[@class='srpResultCard']/div")
-    for element in elements:
-        try:
-            job_title = element.find_element(By.CLASS_NAME, "jobTitle").text
-            company_name = element.find_element(By.CLASS_NAME, "companyName").text
-            skills = [i.text for i in element.find_elements(By.CLASS_NAME, "skillTitle")]
-            while("" in skills):
-                skills.remove("")
-            sub_element = element.find_element(By.CLASS_NAME, "cardBody")
-            job_type = sub_element.find_element(By.XPATH, "div[1]/div[@class='details']").text
-            location = sub_element.find_element(By.XPATH, "div[2]/div[@class='details']").text  
-            st.write(job_title, company_name, skills, job_type, location)
-            count += 1
-        except:
-            pass
-    try:
-        element.find_element(By.CLASS_NAME, "mqfisrp-right-arrow").click()
-    except :
-        break
+
+# drop-down option
+option = st.selectbox('Select a job role?', ("Data Engineer", "Data Analyst", "Data Architect", "Data Scientist", "Machine Learning Engineer"))
+
+# display waiting text with in-build loading graphics until web-scraping and data processing is completed
+with st.spinner('Web-scraping in progress, please wait for few seconds ...'):
+    # get job details as a list by web-scraping, and data cleansing
+    job_lst = get_job_details(option)
+    
+    # roll up extracted data and get required information as induvidual dictionaries
+    company_dict , skill_dict, location_dict, job_type_dict, experience_dict = roll_up_data(job_lst)
+
+    # company data-frame
+    cmp_df = pd.DataFrame(list(company_dict.items()))
+    cmp_df.columns = ['company names', 'vacancy points']
+
+    # skill data-frame
+    skl_df = pd.DataFrame(list(skill_dict.items()))
+    skl_df.columns = ['skills', 'demand points']
+
+    # location data-frame
+    loc_df = pd.DataFrame(list(location_dict.items()))
+    loc_df.columns = ['cities', 'job counts']
+    # add co-ordinates to cities, for plotting
+    loc_df = add_coordinates(loc_df, 'cities')
+    # add overall percentage to cities, for plotting 
+    sum = loc_df['job counts'].sum()
+    loc_df['percent'] = (loc_df['job counts']/sum)*100
+
+    # job-type data-frame
+    jty_df = pd.DataFrame(list(job_type_dict.items()))
+    jty_df.columns = ['job type', 'count']
+
+    # experience data-frame
+    exp_df = pd.DataFrame(list(experience_dict.items()))
+    exp_df.columns = ['job level', 'count']
+
+    # map object creation
+    data = dict(type = 'choropleth', 
+                locations = ['india'], 
+                locationmode = 'country names', 
+                z=[1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0]) 
+    map = gobj.Figure(data = [data])
+    map.update_geos(fitbounds='locations', visible=False)
+    map.update_traces(showscale=False, hoverinfo="text")
+    map.add_trace(go.Scattergeo(
+                lon = loc_df['longitude'],
+                lat = loc_df['latitude'],
+                text = loc_df['cities'],
+                hoverinfo="text",
+                marker = dict(
+                    size = loc_df['percent']+5,
+                    color = '#0083B8'
+                ), hoverlabel=dict(namelength=0)))
+    map.update_layout(
+        plot_bgcolor="rgba(0,0,0,0)")
+
+    # company barchart object 
+    cmp_fig = px.bar(cmp_df, 
+                    x='vacancy points', y='company names', 
+                    orientation='h', title='Top companies',
+                    color_discrete_sequence=["#0083B8"] * len(cmp_df),
+                    template="plotly_white"
+    )
+    cmp_fig.update_layout(
+        plot_bgcolor="rgba(0,0,0,0)",
+        xaxis=(dict(showgrid=False)),
+        yaxis=dict(autorange="reversed")
+    )
+
+    # skills barchart object
+    skl_fig = px.bar(skl_df, 
+                    x='skills', y='demand points', 
+                    orientation='v', title='Top skills',
+                    color_discrete_sequence=["#0083B8"] * len(skl_df),
+                    template="plotly_white",
+    )
+    skl_fig.update_layout(
+        plot_bgcolor="rgba(0,0,0,0)",
+        xaxis=(dict(showgrid=False)),
+        yaxis=(dict(showgrid=False))
+    )
+
+    # location barchart object
+    loc_fig = px.bar(loc_df, 
+                    x='cities', y='job counts', 
+                    orientation='v', title='Top cities',
+                    color_discrete_sequence=["#0083B8"] * len(loc_df),
+                    template="plotly_white",
+    )
+    loc_fig.update_layout(
+        plot_bgcolor="rgba(0,0,0,0)",
+        xaxis=(dict(showgrid=False)),
+        yaxis=(dict(showgrid=False))
+    )
+
+    # experience level pie chart object
+    names = exp_df['job level']
+    values = exp_df['count']
+    exp_fig = px.pie(exp_df, values=values, names=names, title='Job level - ratio')
+    exp_fig.update_layout(plot_bgcolor="rgba(0,0,0,0)")
+
+    # job-type pie chart object
+    names = jty_df['job type']
+    values = jty_df['count']
+    jty_fig = px.pie(jty_df, values=values, names=names, title='Job type - ratio')
+    jty_fig.update_layout(plot_bgcolor="rgba(0,0,0,0)")
+
+# plot chart and map objects
+st.markdown("---")
+
+# separating page into left and right columns
+left_column, right_column = st.columns(2)
+
+# plotting chart and map objects one by one
+left_column.plotly_chart(cmp_fig, use_container_width=True)
+right_column.plotly_chart(skl_fig, use_container_width=True)
+left_column.plotly_chart(loc_fig, use_container_width=True)
+right_column.plotly_chart(map, use_container_width=True)
+left_column.plotly_chart(jty_fig, use_container_width=True)
+right_column.plotly_chart(exp_fig, use_container_width=True)
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+# hide default styling
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+st.markdown("---")
+# plotting ends
